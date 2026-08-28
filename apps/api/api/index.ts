138a147ad437053bbfc0cb943a448470a1f28adf
Deivyson05 <deivysonrssantos@outlook.com>
2026-08-28 16:53:33 -0300

    fix: adiciona cors ao backend

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -7,14 +7,19 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 const server = express();
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-    app.enableCors();
-    await app.init();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  app.enableCors({
+    origin: 'https://combogo-meet.vercel.app', // Ou use true para liberar temporariamente para qualquer origem
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.init();
 }
 
 const bootstrapPromise = bootstrap();
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-    await bootstrapPromise;
-    server(req, res);
+  await bootstrapPromise;
+  server(req, res);
 }
